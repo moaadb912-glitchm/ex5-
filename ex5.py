80e68513da585e3d5504b0f3e40e1c45b1defd0c
Moaad <moaad@gmail.com>
2026-01-28 14:19:07 +0100

    nouvelle modification

---
+++ ex5.py
@@ -0,0 +1,9 @@
+def trouver_maximum(liste):
+    if not liste:
+        return "La liste est vide"
+
+    maximum = liste[0]
+    for nombre in liste:
+        if nombre > maximum:
+            maximum = nombre
+    return maximum
